339f2dac48cb4f74acede6472514b3969942c262
HuySon <huyson201@gmail.com>
2022-09-19 15:15:43 +0700

    create fetch hook

--- src/components/ListBook/ListBook.tsx
+++ src/components/ListBook/ListBook.tsx
@@ -1,30 +1,21 @@
-import React, { useEffect, useState } from 'react'
-// import NovelNewItem from '../NovelNewItem/NovelNewItem'
-import styles from './ListBook.module.scss'
-import classNamesBind from 'classnames/bind'
+import React from 'react'
 import BookItem from './BookItem/BookItem'
 import { Book } from '@src/models/book'
 import bookApi from '@src/apis/book.api'
+import { useFetch } from '@src/hooks/useFetch'
+import { PaginationResponse } from '@src/models'
 
-const cx = classNamesBind.bind(styles)
 
 export interface Props {
 }
 const ListBook = ({ }: Props) => {
-    const [listData, setListData] = useState<Array<Book>>([])
-    useEffect(() => {
-        const fetchData = async () => {
-            let resData = await bookApi.getBookPagination()
-            setListData(resData.result)
-        }
+    const { data, isLoading, error } = useFetch<PaginationResponse<Book>>(async () => bookApi.getBookPagination(), [])
 
-        fetchData()
-    }, [])
     return (
         <>
 
             {
-                listData.map((value) => {
+                data?.result.map((value) => {
                     return (
                         <BookItem book={value} key={`key-${value.slug}`} />
                     )
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { AxiosResponse } from "axios";
+
+type CallBackType<T> = Promise<AxiosResponse<T, any>> | Promise<T>
+
+export const useFetch = <T>(callApi: () => CallBackType<T>, dependency?: React.DependencyList) => {
+    const [data, setData] = useState<T>()
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string>()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true)
+            try {
+                let resData = await callApi()
+                setData(resData)
+            } catch (error: any) {
+                setError(error.response?.data?.message ?? error.message)
+            } finally {
+                setIsLoading(false)
+            }
+
+        }
+        fetchData()
+    }, dependency)
+    return { data, isLoading, error }
+}
